d38d89613f2febc8d181f6b2c59a86f7f262535c
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-01-25 11:42:22 -0500

    update plugin settings.json

--- __init__.py
+++ __init__.py
@@ -11,17 +11,17 @@ class Plugin:
     def register_routes(self, server, **kwargs):
         blueprint = Blueprint(f"{__name__}_bp", __name__, **kwargs["blueprint"])
 
-        @blueprint.route("/form")
+        @blueprint.route("/template_form")
         @server.monitor_requests
         def form():
             return render_template("/form.html", form=Form(request.form))
 
-        @blueprint.route("/devices")
+        @blueprint.route("/template_devices")
         @server.monitor_requests
         def devices():
             return render_template("/devices.html")
 
-        server.register_blueprint(blueprint, url_prefix=kwargs["url_prefix"])
+        server.register_blueprint(blueprint)
 
     def register_endpoints(self, controller):
         @controller.register_endpoint
