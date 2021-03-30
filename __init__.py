9fcad735b6427ad3362947192e941605aff03c52
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-03-30 07:37:40 -0400

    fix plugin

--- __init__.py
+++ __init__.py
@@ -12,12 +12,12 @@ class Plugin:
         blueprint = Blueprint(f"{__name__}_bp", __name__, **kwargs["blueprint"])
 
         @blueprint.route("/template_form")
-        @server.process_requests
+        @server.monitor_requests
         def form():
             return render_template("/form.html", form=Form(request.form))
 
         @blueprint.route("/template_devices")
-        @server.process_requests
+        @server.monitor_requests
         def devices():
             return render_template("/devices.html")
 
